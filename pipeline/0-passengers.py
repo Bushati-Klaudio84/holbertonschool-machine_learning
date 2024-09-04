4b5ee46f7e393a9d3508b6817f406b8dc97add51
Bushati-Klaudio84 <klaudio1@gmail.com>
2024-09-04 17:33:13 +0200

    o

--- pipeline/0-passengers.py
+++ pipeline/0-passengers.py
@@ -10,24 +10,19 @@ def availableShips(passengerCount):
     """
     Returns the list of ships that can hold a given number of passengers
     """
-    url = "http://192.168.10.16:8085/api/getAll"
+    url = "https://swapi-api.hbtn.io/api/starships/"
     r = requests.get(url)
-    return(r)
-    
-"""    json = r.json()
+    json = r.json()
     results = json["results"]
     ships = []
     while json["next"]:
         for res in results:
-            if res["codigo"] == 'n/a' or res["codigo"] == 'unknown':
+            if res["passengers"] == 'n/a' or res["passengers"] == 'unknown':
                 continue
-            if int(res["codigo"].replace(',', '')) >= passengerCount:
+            if int(res["passengers"].replace(',', '')) >= passengerCount:
                 ships.append(res["name"])
         url = json["next"]
         r = requests.get(url)
         json = r.json()
         results = json["results"]
     return ships
-"""
-
-
